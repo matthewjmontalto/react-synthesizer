b88945997be4ce1eead94066fdc94b4ef0e603b7
matthewjmontalto <matthewjmontalto@gmail.com>
2019-07-15 16:19:25 -0400

    Add Ref to Redux DevTools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/index.js'
 
@@ -9,7 +9,11 @@ const middleware = [thunk]
 const store = createStore(
   rootReducer,
   initialState,
-  applyMiddleware(...middleware)
+  compose(
+    applyMiddleware(...middleware),
+    // Redux DevTools Chrome Extension
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 )
 
 export default store
